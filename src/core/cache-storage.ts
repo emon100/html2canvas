20bba3ca4ecf48ad717c4011ee1012d1235caeab
emon100 <emon100@qq.com>
2021-08-23 21:07:59 +0800

    Fix: inline svg image taint canva

--- src/core/cache-storage.ts
+++ src/core/cache-storage.ts
@@ -94,7 +94,7 @@ export class Cache {
             img.onload = () => resolve(img);
             img.onerror = reject;
             //ios safari 10.3 taints canvas with data urls unless crossOrigin is set to anonymous
-            if (isInlineBase64Image(src) || useCORS) {
+            if (isInlineImage(src) || useCORS) {
                 img.crossOrigin = 'anonymous';
             }
             img.src = src;
